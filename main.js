bb9207e0f123eae8e9e2683cf70cb7e6a79427de
Maryam Talal <maryam.talal@bvs-mea.com>
2022-11-12 19:17:11 +0200

    v2

--- main.js
+++ main.js
@@ -31,5 +31,55 @@ window.addEventListener("load", () => {
     })
     
 
-    //
+    let commentCol2 = document.querySelectorAll(".page #supporting-documen table tr:last-child td:first-child")
+    let commentColDiv2 = document.querySelectorAll(".page #supporting-documen table td:first-child div[contenteditable='true']")
+    commentCol2.forEach((comment, i) => {
+        comment.addEventListener("click", (e) => {
+            commentColDiv2[i].focus()
+        })
+    })
+
+
+
+    let pages = document.getElementsByClassName("page")
+    let previousBtn = document.getElementById("previousBtn")
+    let nextBtn = document.getElementById("nextBtn")
+    let pageNo = 1
+    console.log(pages[0])
+    previousBtn.addEventListener("click",(e)=>{
+        console.log("fuck")
+        if(pageNo != 1){
+            pages[pageNo-1].classList.add("hidden")
+            pageNo--;
+            pages[pageNo-1].classList.remove("hidden")
+            if (nextBtn.classList.contains("disabled")) {
+                nextBtn.classList.remove("disabled")
+            }
+            if(pageNo == 1){
+                previousBtn.classList.add("disabled")
+            }
+            else if (previousBtn.classList.contains("disabled")) {
+                previousBtn.classList.remove("disabled")
+            }
+            
+        }
+    })
+    nextBtn.addEventListener("click",(e)=>{
+        if(pageNo < pages.length){
+            
+            pages[pageNo-1].classList.add("hidden")
+            pageNo++;
+            pages[pageNo-1].classList.remove("hidden")
+            if (previousBtn.classList.contains("disabled")) {
+                previousBtn.classList.remove("disabled")
+            }
+            if(pageNo == pages.length){
+                nextBtn.classList.add("disabled")
+            }
+            else if (nextBtn.classList.contains("disabled")) {
+                nextBtn.classList.remove("disabled")
+            }
+        }
+        
+    })
 })
